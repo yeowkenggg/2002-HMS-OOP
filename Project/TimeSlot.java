92a5bdec5088aaae9a884eeeccdc0b39be409f42
yeowkenggg <lonnpurgorous@gmail.com>
2024-10-24 19:51:05 +0800

    TimeSlot.java implementation

--- Project/TimeSlot.java
+++ Project/TimeSlot.java
@@ -1,10 +1,38 @@
 import java.util.*;
 import java.io.*;
-import java.sql.Time;
+import java.time.*;
 
 public class TimeSlot {
 
-	private Date date;
-	private Time time;
+	private LocalDate date;
+	private LocalTime time;
+
+	public TimeSlot(LocalDate date, LocalTime time){
+		this.date = date;
+		this.time = time;
+	}
+
+	public LocalDate getDate(){
+		return date;
+	}
+	public void setDate(LocalDate date){
+		this.date = date;
+	}
+
+	public LocalTime getTime(){
+		return time;
+	}
+
+	public void setTime(LocalTime time){
+		this.time = time;
+	}
+
+	public boolean isSameTimeSlot(TimeSlot other){
+		 return this.date.equals(other.date) && this.time.equals(other.time);
+	}
+
+	public String toString(){
+		return "Date: " +date.toString() + "Time: " + time.toString();
+	}
 
 }
